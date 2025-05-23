c06d5d27df60efcf06a301c8addf703ac90e5203
ShyguyBerlin <caspar.m.klein@gmail.com>
2025-05-23 02:29:54 +0200

    significant improvements to cli tool and testing suite

--- cli_tests.py
+++ cli_tests.py
@@ -1,12 +1,104 @@
-from solvers.greedy import *
-from solvers.wigderson import *
+from solvers.solvers import get_solvers
 import networkx as nx
-import asyncio
+from sys import argv
+from testing_tools import *
+from random import randint
 
-def solve_graph(G : nx.graph, solver,do_print=False,delay=0):
-    print("I am using the delay",delay)
+def print_help():
+    print(f"""     Use: {argv[0]} OPTIONS FILES\n
+        Options:
+        --test-name -n <name> : Set a specific test name which will be printed to the results
+        --timeout -t <time> : Sets a timeout for the test, give time in miliseconds
+        --parser -p <format> : Sets the format the parser should use to read the file
+        --definition-file -d : Assume files given to be in Test definition format, refer to README to learn about the format. 
+        --solver -s <array of sorters> : Set which sorters the test should run, defaults to all of them
+        --output -o <path> : Output to specified path
+          
+    When using definition files, the parameters set through arguments overwrite their counterparts in the definition files.""")
 
-    *_,a = solver(G)
-    print(a)
+def do_test():
+    pass
 
-solve_graph(nx.erdos_renyi_graph(200,0.2),wigdersons_first)
+if __name__=="__main__":
+    
+    use_def_files=False
+    test_name=None
+    timeout=None
+    parser=None
+    output_path=None
+
+    solvers=None
+    files=[]
+
+    if len(argv)<=1:
+        print_help()
+        exit()
+    #solve_graph(nx.erdos_renyi_graph(200,0.2),wigdersons_first)
+
+    i=1
+    while i<len(argv):
+        match(argv[i]):
+            case "--test-name" | "-n":
+                if len(argv) > i+1:
+                    test_name=argv[i+1]
+                    i+=1
+                else:
+                    print("Incorrect amount/layout of arguments")
+                    exit(1)
+            case "--timeout" | "-t":
+                if len(argv) > i+1:
+                    timeout=int(argv[i+1])
+                    i+=1
+                else:
+                    print("Incorrect amount/layout of arguments")
+                    exit(1)
+            case "--parser" | "-p":
+                if len(argv) > i+1:
+                    parser=argv[i+1]
+                    i+=1
+                else:
+                    print("Incorrect amount/layout of arguments")
+                    exit(1)
+            case "--definition-file" | "-d":
+                use_def_files=True
+            case "--solver" | "-s":
+                if len(argv) > i+1:
+                    solvers=argv[i+1].split(",")
+                    i+=1
+                else:
+                    print("Incorrect amount/layout of arguments")
+                    exit(1)
+            case "--output" | "-o":
+                if len(argv) > i+1:
+                    output_path=argv[i+1]
+                    i+=1
+                else:
+                    print("Incorrect amount/layout of arguments")
+                    exit(1)
+            case x:
+                if parser==None and not use_def_files:
+                    print("You need to specify a parser/format to run a test on a data containing file.")
+                files.append((x,x,parser))
+        i+=1
+    print(use_def_files,test_name,timeout,parser,output_path,files)
+    # Set default values for arguments not given:
+    if test_name==None:
+        test_name="Test"
+    if timeout==None:
+        timeout=10000
+    if solvers==None:
+        solvers=get_solvers().keys()
+
+    if len(files)==0:
+        print("No files to run tests on given!")
+        exit(1)
+
+    if not use_def_files:
+        input = Test_input(test_name,timeout,files,solvers)
+        test_result = run_test(input)
+        csv = format_tests_as_csv(test_result)
+        if output_path==None:
+            print(csv)
+        else:
+            with open(output_path,"w") as f:
+                f.write(csv)
---
+++ testing_tools.py
@@ -0,0 +1,143 @@
+import networkx as nx
+from solvers.solvers import get_solvers
+import time
+
+def get_and_parse_file(file_path,parser):
+    match parser:
+        case "gml":
+            return [nx.read_gml(file_path)]
+        case "graphml":
+            return [nx.read_graphml(file_path)]
+        case "edge-list":
+            with open(file_path, "r") as file:
+                graphs=[]
+                for graph_raw in file.read().split("\n\n"):
+                    raw_lines = graph_raw.split("\n")
+                    graph :nx.graph = nx.empty_graph(int(raw_lines[0]))
+                    print("Graph ",graph,graph.nodes())
+                    for i in raw_lines[1:]:
+                        nodes = i.split(" ")
+                        graph.add_edge(int(nodes[0]),int(nodes[1]))
+                    print("Graph ",graph,graph.nodes())
+                    graphs.append(graph)
+                return graphs
+        case "adjacency-matrix":
+            with open(file_path, "r") as file:
+                graphs=[]
+                for graph_raw in file.read().split("\n\n"):
+                    if graph_raw == "":
+                        continue
+                    raw_lines = graph_raw.split("\n")
+                    graph :nx.graph = nx.empty_graph(len(raw_lines))
+                    for u in range(len(raw_lines)):
+                        for v in range(len(raw_lines)):
+                            if int(raw_lines[u].split(" ")[v])!=0:
+                                graph.add_edge(u,v)
+                    graphs.append(graph)
+                return graphs
+        case "pickle":
+            G = nx.read_gpickle(file_path)
+            return [G]
+        case "graph6":
+            G = nx.from_graph6_bytes(file_path)
+            return [G]
+        case x:
+            if x is None:
+                print(f"""No parser specified. Please use one of the following formats:
+                gml graphml edge-list adjacency-matrix pickle graph6""")
+            else:
+                print(f"""The format "{x}" is not supported. Please use one of the following formats:
+                gml graphml edge-list adjacency-matrix pickle graph6""")
+            exit(1)
+
+class Test_input:
+    def __init__(self,test_name,timeout,raw_graph_sets,solvers):
+        self.test_name=test_name
+        self.timeout=timeout
+        self.graph_sets=[]
+        for (file_path,set_name,parser) in raw_graph_sets:
+            self.graph_sets.append(Graph_set(parser,file_path,set_name))
+        self.solvers=solvers
+
+class Graph_set:
+    def __init__(self,parser,file_path,set_name):
+        self.graphs=get_and_parse_file(file_path,parser)
+        self.name=set_name
+
+class Graph_set_result:
+    def __init__(self,graph_set_name):
+        self.name=graph_set_name
+        self.orders=[]
+        self.colors=[]
+        self.exec_times=[]
+
+    def add_result(self,order:int,colors:int,exec_time:float):
+        self.orders.append(order)
+        self.colors.append(colors)
+        self.exec_times.append(exec_time)
+
+class Test_result:
+    test_name=None
+    
+    results : list[tuple[Graph_set_result,str]]=[]
+
+    def add_results(self,solver_used:str,graph_set_results:list[Graph_set_result]):
+        modified_results=[ (i,solver_used) for i in graph_set_results]
+        self.results.extend(modified_results)
+
+
+CSV_HEADER="test_name,solver,graph_set,graph_idx,graph_order_min,graph_order_max,graph_order_avg,graph_order_med,exec_time_min,exec_time_max,exec_time_avg,exec_time_med,colors_min,colors_max,colors_avg,colors_med"
+
+def format_tests_as_csv(test : Test_result) -> str:
+    csv_lines=[CSV_HEADER]
+
+    for graph_set_result,solver in test.results:
+        min_order = min(graph_set_result.orders)
+        max_order = max(graph_set_result.orders)
+        avg_order = sum(graph_set_result.orders)/len(graph_set_result.orders)
+        med_order = sorted(graph_set_result.orders)[len(graph_set_result.orders)//2]
+
+        min_colors = min(graph_set_result.colors)
+        max_colors = max(graph_set_result.colors)
+        avg_colors = sum(graph_set_result.colors)/len(graph_set_result.colors)
+        med_colors = sorted(graph_set_result.colors)[len(graph_set_result.colors)//2]
+
+        min_exec_time = min(graph_set_result.exec_times)
+        max_exec_time = max(graph_set_result.exec_times)
+        avg_exec_time = sum(graph_set_result.exec_times)/len(graph_set_result.exec_times)
+        med_exec_time = sorted(graph_set_result.exec_times)[len(graph_set_result.exec_times)//2]
+
+        csv_lines.append(f"{test.test_name},{solver},{graph_set_result.name},0,{min_order},{max_order},{avg_order},{med_order},{min_exec_time},{max_exec_time},{avg_exec_time},{med_exec_time},{min_colors},{max_colors},{avg_colors},{med_colors}")
+    return "\n".join(csv_lines)
+
+#Returns (Runtime in miliseconds, Number of colors used)
+def solve_graph(G : nx.graph, solver) -> tuple[float,int]:
+    start_time = time.monotonic_ns()
+
+    *_,a = solver(G)
+    
+    # Calculate difference, devide by 
+    execution_time = (time.monotonic_ns() - start_time)/1_000_000
+
+    return (execution_time,max(a.values()))
+
+def run_test(input : Test_input) -> Test_result:
+    res=Test_result()
+    res.test_name=input.test_name
+
+    for solver in input.solvers:
+        if not solver in get_solvers():
+            print(f"Solver {solver} not found!")
+            continue
+
+        solver_results = []
+        for graph_set in input.graph_sets:
+            graph_set : Graph_set = graph_set
+            graph_set_result = Graph_set_result(graph_set.name)
+            for graph in graph_set.graphs:
+                graph : nx.Graph = graph
+                time, cols = solve_graph(graph,get_solvers()[solver])
+                graph_set_result.add_result(graph.order(),cols,time)
+            solver_results.append(graph_set_result)
+        res.add_results(solver,solver_results)
+    return res
