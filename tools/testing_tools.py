f86352f02ec9ca7d0f21876118d79bdf7d53e5de
Caspar Klein <caspar.m.klein@gmail.com>
2025-06-13 16:11:47 +0200

    add density to csv output

--- tools/testing_tools.py
+++ tools/testing_tools.py
@@ -118,11 +118,13 @@ class Graph_set_result:
     def __init__(self,graph_set_name):
         self.name=graph_set_name
         self.orders=[]
+        self.densities=[]
         self.colors=[]
         self.exec_times=[]
 
-    def add_result(self,order:int,colors:int,exec_time:float):
+    def add_result(self,order:int,density:float,colors:int,exec_time:float):
         self.orders.append(order)
+        self.densities.append(density)
         self.colors.append(colors)
         self.exec_times.append(exec_time)
 
@@ -136,7 +138,7 @@ class Test_result:
         self.results.extend(modified_results)
 
 
-CSV_HEADER="test_name,solver,graph_set,graph_idx,graph_order_min,graph_order_max,graph_order_avg,graph_order_med,exec_time_min,exec_time_max,exec_time_avg,exec_time_med,colors_min,colors_max,colors_avg,colors_med"
+CSV_HEADER="test_name,solver,graph_set,graph_idx,graph_order_min,graph_order_max,graph_order_avg,graph_order_med,graph_density_min,graph_density_max,graph_density_avg,graph_density_med,exec_time_min,exec_time_max,exec_time_avg,exec_time_med,colors_min,colors_max,colors_avg,colors_med"
 
 def format_tests_as_csv(tests : list[Test_result]) -> str:
     csv_lines=[CSV_HEADER]
@@ -146,6 +148,11 @@ def format_tests_as_csv(tests : list[Test_result]) -> str:
             max_order = max(graph_set_result.orders)
             avg_order = sum(graph_set_result.orders)/len(graph_set_result.orders)
             med_order = sorted(graph_set_result.orders)[len(graph_set_result.orders)//2]
+
+            min_density = min(graph_set_result.densities)
+            max_density = max(graph_set_result.densities)
+            avg_density = sum(graph_set_result.densities)/len(graph_set_result.densities)
+            med_density = sorted(graph_set_result.densities)[len(graph_set_result.densities)//2]
 
             min_colors = min(graph_set_result.colors)
             max_colors = max(graph_set_result.colors)
@@ -157,7 +164,7 @@ def format_tests_as_csv(tests : list[Test_result]) -> str:
             avg_exec_time = sum(graph_set_result.exec_times)/len(graph_set_result.exec_times)
             med_exec_time = sorted(graph_set_result.exec_times)[len(graph_set_result.exec_times)//2]
 
-            csv_lines.append(f"\"{test.test_name}\",\"{solver}\",\"{graph_set_result.name}\",0,{min_order},{max_order},{round(avg_order,2)},{med_order},{round(min_exec_time,4)},{round(max_exec_time,4)},{round(avg_exec_time,4)},{round(med_exec_time,4)},{round(min_colors,4)},{round(max_colors,4)},{round(avg_colors,4)},{round(med_colors,4)}")
+            csv_lines.append(f"\"{test.test_name}\",\"{solver}\",\"{graph_set_result.name}\",0,{min_order},{max_order},{round(avg_order,2)},{med_order},{round(min_density,4)},{round(max_density,4)},{round(avg_density,4)},{round(med_density,4)},{round(min_exec_time,4)},{round(max_exec_time,4)},{round(avg_exec_time,4)},{round(med_exec_time,4)},{round(min_colors,4)},{round(max_colors,4)},{round(avg_colors,4)},{round(med_colors,4)}")
     return "\n".join(csv_lines)
 
 def format_test_as_csv(test : Test_result) -> str:
@@ -203,7 +210,7 @@ def run_test(input : Test_input) -> Test_result:
                     graph_avg_exec_time+=exec_time
                     graph_avg_cols+=cols
                     steps_completed+=1
-                graph_set_result.add_result(graph.order(),graph_avg_cols/input.repetitions,graph_avg_exec_time/input.repetitions)
+                graph_set_result.add_result(graph.order(),nx.function.density(graph),graph_avg_cols/input.repetitions,graph_avg_exec_time/input.repetitions)
                 if (do_logging and time.time()-last_log>5):
                     print(f"Done by {round(steps_completed/total_steps*100,1)}% est. time remaining: {round((time.time()-start_time)/steps_completed*(total_steps-steps_completed),1)}s")
                     last_log=time.time()
