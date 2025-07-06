e35f3099a2be1389b89c51e55f380d7d680af8f3
ShyguyBerlin <caspar.m.klein@gmail.com>
2025-07-06 17:45:51 +0200

    add johnson_naive, a variant which does not sort nodes before creating independent set

--- solvers/Johanson/johanson.py
+++ solvers/Johanson/johanson.py
@@ -27,3 +27,25 @@ def johnson(G : nx.Graph):
             surching = 0
         yield labels
     yield labels
+
+# Same as johnson, but indipendent set does not regard the node degrees
+def johnson_naive(G : nx.Graph):
+ 
+    G=G.copy()
+
+    labels={}
+    colors = 1
+    surching = 1
+
+    
+
+    while(surching == 1):
+        indi = iset.indiset_naive(G)
+        G.remove_nodes_from(indi)
+        for i in indi:
+            labels[i]=colors
+        colors = colors + 1
+        if (G.number_of_nodes() == 0):
+            surching = 0
+        yield labels
+    yield labels
