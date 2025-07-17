a2040efb9569a698228e094c5ff6668cf26d2f99
ShyguyBerlin <caspar.m.klein@gmail.com>
2025-07-17 16:34:59 +0200

    fix make_better functions

--- solvers/greedy_better.py
+++ solvers/greedy_better.py
@@ -4,34 +4,34 @@ from .locel_surch import make_better_coloring as mbc
 from . import greedy as gy
 
 def greedy_no_sort_make_better( G : nx.graph):
-  labels = {}
-  
-  labels = gy.greedy_no_sort(G)
-  labels = mbc.make_batter_coloring(G, labels)
+    labels = {}
 
-  yield labels
+    *_,labels = gy.greedy_no_sort(G)
+    labels = mbc.make_better_coloring(G, labels)
+
+    yield labels
 
 def greedy_asc_deg_make_better( G : nx.graph):
     labels={}
 
-    labels = gy.greedy_asc_deg(G)
-    labels = mbc.make_batter_coloring(G, labels)
+    *_,labels = gy.greedy_asc_deg(G)
+    labels = mbc.make_better_coloring(G, labels)
         
     yield labels
 
 def greedy_desc_deg_make_better( G : nx.graph):
     labels={}
 
-    labels = gy.greedy_desc_deg(G)
-    labels = mbc.make_batter_coloring(G, labels)
+    *_,labels = gy.greedy_desc_deg(G)
+    labels = mbc.make_better_coloring(G, labels)
   
     yield labels
 
 def greedy_color_swaps_make_better( G : nx.graph):
     labels = {}
     
-    labels = gy.greedy_color_swaps(G)
+    *_,labels = gy.greedy_color_swaps(G)
       
-    labels = mbc.make_batter_coloring(G, labels)
+    labels = mbc.make_better_coloring(G, labels)
 
     yield labels
