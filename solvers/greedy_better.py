fa7297bbc980619c2e1831fce80e4c6d4629f85d
AliciaMirror <152632384+AliciaMirror@users.noreply.github.com>
2025-07-17 16:05:19 +0200

    Create greedy_better.py

---
+++ solvers/greedy_better.py
@@ -0,0 +1,36 @@
+import networkx as nx
+from random import shuffle
+import make_better_coloring as mbc
+import greedy as gy
+
+def greedy_no_sort_make_better( G : nx.graph):
+
+  labels = gy.greedy_no_sort(G)
+  labels = mbc.make_batter_coloring(G, labels)
+
+  yield labels
+
+def greedy_asc_deg_make_better( G : nx.graph):
+    labels={}
+
+    labels = gy.greedy_asc_deg(G)
+    labels = mbc.make_batter_coloring(G, labels)
+        
+    yield labels
+
+def greedy_desc_deg_make_better( G : nx.graph):
+    labels={}
+
+    labels = gy.greedy_desc_deg(G)
+    labels = mbc.make_batter_coloring(G, labels)
+  
+    yield labels
+
+def greedy_color_swaps_make_better( G : nx.graph):
+    labels = {}
+    
+    labels = gy.greedy_color_swaps(G)
+      
+    labels = mbc.make_batter_coloring(G, labels)
+
+    yield labels
